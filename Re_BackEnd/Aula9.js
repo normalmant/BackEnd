59168b9a5a4409c1f4447504bf554ae64e2a9be0
Renato <114404480+normalmant@users.noreply.github.com>
2026-04-27 08:56:14 -0300

    Add files via upload

---
+++ Re_BackEnd/Aula9.js
@@ -0,0 +1,106 @@
+const funcionario =[
+    {
+    nome: "Roberto",
+    idade: 18,
+    cargo: "faxineiro",
+    pagamento: 1500,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Miguel",
+    idade: 18,
+    cargo: "Gerente",
+    pagamento: 15000,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Helena",
+    idade: 28,
+    cargo: "Marketing",
+    pagamento: 1000,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Gael",
+    idade: 38,
+    cargo: "Desenvolvedor",
+    pagamento: 5000,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Davi",
+    idade: 19,
+    cargo: "secretário",
+    pagamento: 1200,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Mavi",
+    idade: 48,
+    cargo: "faxineiro",
+    pagamento: 2000,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Cecilia",
+    idade: 88,
+    cargo: "Desenvolvedor",
+    pagamento: 1500,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Fernando",
+    idade: 58,
+    cargo: "CEO",
+    pagamento: 10000,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Bruna",
+    idade: 78,
+    cargo: "Conzinheira",
+    pagamento: 2500,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+{
+    nome: "Jonas",
+    idade: 10,
+    cargo: "Massagista",
+    pagamento: 2,
+    contrato: "2 anos",
+    escala: "6/1",
+
+},
+
+]
+const funcionariosJSON = JSON.stringify(funcionario)
+
+
+ const fs = require("fs")
+ fs.writeFileSync("funcionarios.json", funcionariosJSON)
+
+ const funcionariosArquivo = require("./funcionarios.json")
+ console.log(funcionariosArquivo)
+
+funcionariosArquivo.forEach((trabalhador)=>{
+    console.log(trabalhador.nome+ " idade: "+ trabalhador.idade+ " anos   Cargo: "+ trabalhador.cargo+ "   Salário: R$"+trabalhador.pagamento)
+    
+})
